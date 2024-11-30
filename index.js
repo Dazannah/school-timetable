488f39e2547b3e64d61596366733a388fd95dfd2
Dazannah <beattius@gmail.com>
2024-11-30 09:14:58 +0100

    updated class list and added flash message

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-function main() {
-  generateTables()
-}
+// prettier-ignore
+const classList = ["Magyar nyelv és irodalom",
+  "Magyar nyelv és irodalom",
+  "Angol",
+  "Német",
+  "Matematika",
+  "Történelem, társadalmi és állampolgári ismeretek",
+  "Etika",
+  "Biológia-egészségtan",
+  "Fizika",
+  "Kémia",
+  "Földrajz",
+  "Ének-zene",
+  "Dráma és tánc",
+  "Vizuális kultúra",
+  "Mozgóképkultúra és médiaismeret",
+  "Művészetek",
+  "Informatika",
+  "Technika, életvitel és gyakorlat",
+  "Testnevelés és sport"
+]
 
-window.addEventListener("DOMContentLoaded", () => {
-  main()
-})
+function main() {
+  generateTables(classList)
+
+  loadSavedButtons()
+}
 
 function preventDefault(e) {
   e.preventDefault()
@@ -18,5 +38,15 @@ function drop(e) {
   e.preventDefault()
   var data = e.dataTransfer.getData("text")
 
-  if (!e.target.children[0] && e.target.tagName == "TH") e.target.appendChild(document.getElementById(data))
+  if (!e.target.children[0] && e.target.tagName == "TH") {
+    e.target.appendChild(document.getElementById(data))
+
+    return
+  }
+
+  flashAlert("Ez a mező már foglalt!")
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+  main()
+})
